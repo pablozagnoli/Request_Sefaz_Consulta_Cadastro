fede66a2835653863cf924430eb40577a947b6ee
Pablo Zagnoli <68761353+pablozagnoli@users.noreply.github.com>
2021-08-10 22:42:24 -0300

    Add files via upload

---
+++ Consulta_Sefaz_Cadastro/Consulta_Sefaz_Cadastro/Program.cs
@@ -0,0 +1,13 @@
+﻿using Consulta_Sefaz_Cadastro.Request;
+
+
+namespace Consulta_Sefaz_Cadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Request_Sefaz.Reques_Sergipe();
+        }
+    }
+}
