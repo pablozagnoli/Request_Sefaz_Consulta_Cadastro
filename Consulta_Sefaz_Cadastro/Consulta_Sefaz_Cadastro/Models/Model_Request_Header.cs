2f871fe4c5e2e15ccea2f606a552fb69a8974e23
Pablo Zagnoli <68761353+pablozagnoli@users.noreply.github.com>
2021-08-10 22:40:17 -0300

    Add files via upload

---
+++ Consulta_Sefaz_Cadastro/Consulta_Sefaz_Cadastro/Models/Model_Request_Header.cs
@@ -0,0 +1,12 @@
+﻿namespace Consulta_Sefaz_Cadastro.Models
+{
+    public class Model_Request_Header
+    {
+        public string AppName { get; set; }
+        public string TransId { get; set; }
+        public string cdCnpj { get; set; }
+        public string cdPessoaContribuinte { get; set; }
+        public string dsImagem { get; set; }
+        public string cdImagem { get; set; }
+    }
+}
